aacd85edd3809874ead3ec87c485bf84957a3bfe
Ralf Meyer <meyer.ralf@yahoo.com>
2022-05-26 14:38:10 -0400

    Adjust naming in test

--- tests/test_orbital_diagrams.py
+++ tests/test_orbital_diagrams.py
@@ -1,9 +1,9 @@
-from tmc_tools.orbital_energies import orbital_energies
+from tmc_tools.orbital_energies import get_d_orbital_energies
 
 
 def test_orbital_diagram(resource_path_root):
 
-    energ_a, energ_b = orbital_energies(
+    energ_a, energ_b = get_d_orbital_energies(
         resource_path_root / 'crf6_fixed.molden')
 
     assert len(energ_a) == 5
