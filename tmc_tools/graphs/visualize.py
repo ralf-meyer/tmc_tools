b93d8babaac71ee1e7bb641521b5872bec67e04f
Ralf Meyer <meyer.ralf@yahoo.com>
2022-06-21 12:08:56 -0400

    Add graph visualization tool

---
+++ tmc_tools/graphs/visualize.py
@@ -0,0 +1,8 @@
+import networkx as nx
+from tmc_tools.constants import atomic_numbers, jmol_colors
+
+
+def draw_graph(graph, **kwargs):
+    node_color = [jmol_colors[atomic_numbers[sym]]
+                  for _, sym in graph.nodes().data('symbol')]
+    nx.draw(graph, node_color=node_color, edgecolors='k', **kwargs)
